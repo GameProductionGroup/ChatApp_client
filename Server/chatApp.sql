8e21ec93f3dbe317b2642b408dd39bfcc8c2e504
kazuma_ohura <k.ohura0303@gmail.com>
2020-08-13 16:37:12 +0900

    [#7] アカウント作成の送信部分を作成

---
+++ Server/chatApp.sql
@@ -0,0 +1,42 @@
+-- Create a new database called 'DatabaseName'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'ChatApp'
+)
+CREATE DATABASE ChatApp
+GO
+
+-- Create a new table called 'account' in schema 'SchemaName'
+-- Drop the table if it already exists
+IF OBJECT_ID('SchemaName.TableName', 'U') IS NOT NULL
+DROP TABLE SchemaName.TableName
+GO
+-- Create the table in the specified schema
+CREATE TABLE SchemaName.TableName
+(
+    TableNameId INT NOT NULL PRIMARY KEY, -- primary key column
+    Column1 [NVARCHAR](50) NOT NULL,
+    Column2 [NVARCHAR](50) NOT NULL
+    -- specify more columns here
+);
+GO
+
+-- Create a new table called 'content' in schema 'SchemaName'
+-- Drop the table if it already exists
+IF OBJECT_ID('SchemaName.TableName', 'U') IS NOT NULL
+DROP TABLE SchemaName.TableName
+GO
+-- Create the table in the specified schema
+CREATE TABLE SchemaName.TableName
+(
+    TableNameId INT NOT NULL PRIMARY KEY, -- primary key column
+    Column1 [NVARCHAR](50) NOT NULL,
+    Column2 [NVARCHAR](50) NOT NULL
+    -- specify more columns here
+);
+GO
